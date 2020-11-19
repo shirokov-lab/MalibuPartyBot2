acc4d7615b76856f9766b061a32dda04830bf60f
shirokov-web <shirokov.web@gmail.com>
2020-11-19 15:08:22 +0100

    add lamej sonya

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import json
 TOKEN = os.getenv('TOKEN')
 PAY_TOKEN = os.getenv('PAY_TOKEN')
-ADMINS_ID = [454709994, 685001761, 693267094]
+ADMINS_ID = [454709994, 685001761, 693267094, 200062478]
 
 TEXTS={}
 with open('text/texta.json',encoding='utf8') as file:
